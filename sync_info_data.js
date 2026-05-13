248a966abd21703574ac0cd76625bd2f9192a5da
lucasnguynn <nguyendangquocdat.forwork@gmail.com>
2026-05-14 01:17:42 +0700

    ci: switch cron workflow to sync_all_data and remove deprecated scripts

--- sync_info_data.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-console.error('[DEPRECATED] sync_info_data.js has been replaced by sync_all_data.js.');
-console.error('Please update cron/workflows to run: node sync_all_data.js');
-process.exit(1);
