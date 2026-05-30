2b8f62f9eb23df3b65f90c4e598de00fcb8c5460
GitHub Action <action@github.com>
2026-05-30 22:53:19 +0000

    chore: auto-update data and stamp PWA version [skip ci]

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
  * - Query-string-safe matching for cache-busted URLs (?v=timestamp).
  */
 
-const SW_VERSION = '20260530215639';
+const SW_VERSION = '20260530225319';
 const STATIC_CACHE = `vgumap-static-${SW_VERSION}`;
 const DATA_CACHE = `vgumap-data-${SW_VERSION}`;
 
