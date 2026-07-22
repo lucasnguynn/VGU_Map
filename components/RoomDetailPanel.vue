b9170c8c28b6ca30523dc628be4957828a349d4c
Lê Hồ Thu Giang <106240409@student.vgu.edu.vn>
2026-07-22 12:48:00 +0700

    Update RoomDetailPanel.vue

--- components/RoomDetailPanel.vue
+++ components/RoomDetailPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="room-detail-panel">
+  <div class="room-detail-panel" v-if="isOpen">
     <!-- Nút Đóng -->
     <button class="close-btn" @click="closePanel">
       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
